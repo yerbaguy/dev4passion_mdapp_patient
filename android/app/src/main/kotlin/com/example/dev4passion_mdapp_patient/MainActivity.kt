28805f24550dadbb2d1235e94da4d88395079801
bartosz <bartosz@bartoszs-MacBook-Pro.local>
2023-11-01 13:57:40 +0100

    dev4passion mdapp update

---
+++ android/app/src/main/kotlin/com/example/dev4passion_mdapp_patient/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dev4passion_mdapp_patient
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/home/home.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+//import 'package:dev4passion_mdapp_/screens/drawer/Sidemenu.dart';
+
+class Home extends StatefulWidget {
+  const Home({ Key? key }) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+   return Scaffold(
+      // drawer: Sidemenu(),
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        // title: Text(widget.title),
+        title: Text("HOME"),
+        backgroundColor: Colors.pinkAccent,
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+        
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:', style: TextStyle(fontSize: 40),
+            ),
+           
+          ],
+        ),
+      ),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: _incrementCounter,
+      //   tooltip: 'Increment',
+      //   child: const Icon(Icons.add),
+      // ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+    //   floatingActionButton: FloatingActionButton(
+    //     onPressed: _incrementCounter,
+    //     tooltip: 'Increment',
+    //     child: const Icon(Icons.add),
+    //   ), // This trailing comma makes auto-formatting nicer for build methods.
+    // );
+    //   floatingActionButton: FloatingActionButton(
+    //     onPressed: _incrementCounter,
+    //     tooltip: 'Increment',
+    //     child: const Icon(Icons.add),
+    //   ), // This trailing comma makes auto-formatting nicer for build methods.
+    // );
+
+  }
+}
